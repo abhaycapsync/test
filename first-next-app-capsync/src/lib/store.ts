1af779d8a3f8798008d25b1c6a8581f9fe129d6b
abhaycapsync <161013637+abhaycapsync@users.noreply.github.com>
2024-02-23 15:52:25 +0530

    remove persistant store

--- first-next-app-capsync/src/lib/store.ts
+++ first-next-app-capsync/src/lib/store.ts
@@ -1,21 +1,13 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import CounteSlice from "./Sliecs/CounteSlice";
-import storage from "redux-persist/lib/storage";
-import { persistReducer, persistStore } from "redux-persist";
 
-const persistConfig = {
-  key: "root",
-  storage,
+export const makeStore = () => {
+  return configureStore({
+    reducer: {
+      counter: CounteSlice,
+    },
+  });
 };
-const rootReducer = combineReducers({
-  counter: CounteSlice,
-});
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-export const makeStore: any = configureStore({
-  reducer: persistedReducer,
-});
-export const persistor = persistStore(makeStore);
 
 export type AppStore = ReturnType<typeof makeStore>;
 export type RootState = ReturnType<AppStore["getState"]>;
